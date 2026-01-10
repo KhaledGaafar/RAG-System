50c3626f800bd3675d256e0b2f5fd737e1fc13c7
Khaledgaafar <khaledgafaar@gmail.com>
2026-01-11 01:33:15 +0200

    Enhance database configuration

--- RAGChat/RAGChat/settings.py
+++ RAGChat/RAGChat/settings.py
@@ -113,9 +113,13 @@ ASGI_APPLICATION = 'RAGChat.asgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
